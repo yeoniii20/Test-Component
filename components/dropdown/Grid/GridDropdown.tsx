f9419eaad91db11b47b60ebbb8ed5e24c685b481
KwonSuyeon <commetoi_yeoni@naver.com>
2023-06-22 00:48:57 +0900

    feat: confirm, cancel 버튼 추가

--- components/dropdown/Grid/GridDropdown.tsx
+++ components/dropdown/Grid/GridDropdown.tsx
@@ -19,6 +19,9 @@ const GridDropdown = () => {
   const [selectedData, setSelectedData] = useState<TableDataItem[]>([]);
   const [selectAll, setSelectAll] = useState(false);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [dropdownMenu, setDropdownMenu] = useState<boolean>(false);
+
+  const selectedServerNames = selectedData.map((option) => option.server);
 
   const handleCheckboxChange = (item: TableDataItem) => {
     const isChecked = selectedData.some(
@@ -60,87 +63,167 @@ const GridDropdown = () => {
     setSearchTerm("");
   };
 
+  const dropdownText =
+    selectedData.length > 0
+      ? `${selectedServerNames[0]}외 ${selectedData.length - 1}건`
+      : "EMS server를 선택하세요";
+
+  const handleDropdownVisible = () => {
+    setSelectedData([]);
+    setSearchTerm("");
+    setDropdownMenu(!dropdownMenu);
+  };
+
+  const handleCancelClick = () => {
+    setSelectedData([]);
+    setDropdownMenu(false);
+  };
+
+  const handleConfirm = () => {
+    const selectedCount = selectedData.length;
+
+    if (selectedCount > 0) {
+      setSelectedData(selectedData);
+      const selectedServerNames = selectedData.map(
+        (item: TableDataItem) => item.server
+      );
+      alert(
+        `서버 ${selectedCount}건을 선택했습니다. 선택한 서버: ${selectedServerNames.join(
+          ", "
+        )}`
+      );
+    } else {
+      setSelectedData([]);
+      alert("No servers selected");
+    }
+
+    setDropdownMenu(false);
+
+    console.log(selectedData);
+  };
+
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search"
-        value={searchTerm}
-        onChange={handleSearchChange}
-        className="p-2 mb-2 border"
-        style={{ color: "black" }}
-      />
-      <button
-        style={{ backgroundColor: "lightgreen", height: 41, width: 60 }}
-        onClick={clearSearch}
+      {/* 입력 창 */}
+      <div
+        onClick={handleDropdownVisible}
+        style={{
+          backgroundColor: "lightGrey",
+          width: 255,
+          height: 40,
+          cursor: "pointer",
+        }}
       >
-        Clear
-      </button>
-      <table className="w-full border-collapse">
-        <thead>
-          <tr style={{ backgroundColor: "grey" }}>
-            <th className="w-12">
-              <input
-                type="checkbox"
-                checked={selectAll}
-                onChange={handleSelectAllChange}
-              />
-            </th>
-            <th
-              className="border-b"
-              colSpan={2}
-              style={{
-                color: "black",
-                paddingLeft: 10,
-                borderLeft: "2px solid black",
-                backgroundColor: "grey",
-              }}
-            >
-              Server
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((item, index) => (
-            <tr
-              key={item.id}
-              style={{
-                backgroundColor: index % 2 === 0 ? "lightGrey" : "grey",
-              }}
-            >
-              <td className="text-center">
-                <input
-                  type="checkbox"
-                  checked={checkboxChecked(item)}
-                  onChange={() => handleCheckboxChange(item)}
-                />
-              </td>
-              <td
-                className="border-b"
-                colSpan={2}
-                style={{
-                  color: "black",
-                  paddingLeft: 10,
-                  borderLeft: "2px solid black",
-                }}
-              >
-                {item.server}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-
-        {selectedData.length > 0 && (
-          <div className="mt-4">
-            <h4 style={{ color: "black" }}>Selected Servers:</h4>
-            <ul>
-              {selectedData.map((item) => (
-                <li key={item.id}>{item.server}</li>
+        {dropdownText}
+      </div>
+      {dropdownMenu ? (
+        <div>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className="p-2 mb-2 border"
+            style={{ color: "black" }}
+          />
+          <button
+            style={{ backgroundColor: "black", height: 41, width: 60 }}
+            onClick={clearSearch}
+          >
+            Clear
+          </button>
+          <table className="w-full border-collapse">
+            <thead>
+              <tr style={{ backgroundColor: "grey" }}>
+                <th className="w-2">
+                  <input
+                    type="checkbox"
+                    checked={selectAll}
+                    onChange={handleSelectAllChange}
+                  />
+                </th>
+                <th
+                  className="border-b"
+                  colSpan={2}
+                  style={{
+                    color: "black",
+                    paddingLeft: 10,
+                    borderLeft: "2px solid black",
+                    backgroundColor: "grey",
+                  }}
+                >
+                  Server
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData.map((item, index) => (
+                <tr
+                  key={item.id}
+                  style={{
+                    backgroundColor: index % 2 === 0 ? "lightGrey" : "grey",
+                  }}
+                >
+                  <td className="text-center">
+                    <input
+                      type="checkbox"
+                      checked={checkboxChecked(item)}
+                      onChange={() => handleCheckboxChange(item)}
+                    />
+                  </td>
+                  <td
+                    className="border-b"
+                    colSpan={2}
+                    style={{
+                      color: "black",
+                      paddingLeft: 10,
+                      borderLeft: "2px solid black",
+                    }}
+                  >
+                    {item.server}
+                  </td>
+                </tr>
               ))}
-            </ul>
-          </div>
-        )}
-      </table>
+            </tbody>
+            <div style={{ display: "flex" }}>
+              <button
+                style={{
+                  backgroundColor: "black",
+                  height: 41,
+                  width: 60,
+                  borderRadius: 10,
+                }}
+                onClick={handleConfirm}
+              >
+                Confirm
+              </button>
+              <button
+                style={{
+                  backgroundColor: "black",
+                  height: 41,
+                  width: 60,
+                  borderRadius: 10,
+                }}
+                onClick={handleCancelClick}
+              >
+                Cancel
+              </button>
+            </div>
+            {selectedData.length > 0 && (
+              <div className="mt-4">
+                <h4 style={{ color: "black" }}>Selected Servers:</h4>
+                <ul>
+                  {selectedData.map((item) => (
+                    <li key={item.id}>{item.server}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </table>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
