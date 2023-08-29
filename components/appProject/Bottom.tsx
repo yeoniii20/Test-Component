e1eb8d903e3d173d69655b8b0fdc890f83d55d0f
KwonSuyeon <commetoi_yeoni@naver.com>
2023-08-30 00:57:05 +0900

    feat: top, bottom 컴포넌트 생성

---
+++ components/appProject/Bottom.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const Bottom = () => {
+  return (
+    <>
+      <div style={{}}>
+        <div
+          style={{
+            borderRadius: "24px 24px 0px 0px",
+            borderRight: "1.5px solid #1F1F1F",
+            borderTop: "1.5px solid  #1F1F1F",
+            borderLeft: "1.5px solid #1F1F1F",
+            background: "#FFF",
+            // height: 88,
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 20,
+              placeContent: "center",
+              paddingTop: 52,
+              paddingBottom: 12,
+            }}
+          ></div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Bottom;
