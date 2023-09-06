8e603e4c630016b9af3c987bfc500cc376203ec9
KwonSuyeon <commetoi_yeoni@naver.com>
2023-09-06 23:24:24 +0900

    fix: 컴포넌트 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,9 @@
 import Kakao from "@/components/appProject/Login/kakao";
 import SiginIn from "@/components/appProject/Login/siginIn";
 import Message from "@/components/appProject/Modal/message";
-import Note from "@/components/appProject/spendingStatus/note";
-import Statistics from "@/components/appProject/spendingStatus/statistics";
+import FriendsList from "@/components/appProject/Page/friendsList";
+import Note from "@/components/appProject/Page/note";
+import Statistics from "@/components/appProject/Page/statistics";
 
 const Login = () => {
   return (
@@ -37,6 +38,7 @@ const Login = () => {
         >
           <Statistics />
           <Note />
+          <FriendsList />
           <Message />
         </div>
       </div>
